00f6f177e988bc497c407403fd2eaf04615f676e
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-03-29 20:25:37 +0300

    first changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,30 @@
+import javax.management.MBeanRegistration
+
 fun main() {
-
-    names(listOf("ruthless","better","care","for","the","soul","hurts","adapt","used","strong") )
-
-    var x = Person()
-    var person2 =("Carol",15,1.46,43.4)
-    var person3 =Person("Loise",19,2.34,50.4)
-    var person4 =Person("Amina",18,2.45,60.2)
-    var friendsList= mutableListOf(person1,person2,person3,person4)
+    namesList(listOf("ruthless","better","care","for","the","soul","hurts","adapt","used","strong") )
+    heights()
+    var person1 = Person("Jem",30,5.0,80)
+    var person2 =Person("Carol",15,1.46,43)
+    var person3 =Person("Loise",19,2.34,50)
+    var person4 =Person("Amina",18,2.45,60)
+    var friendsList= listOf<Person>(person1,person2,person3,person4)
     var friendslist=friendsList.sortedByDescending{Person->Person.age }
     println(friendslist)
+
 
 
 }
 //Given a list of 10 strings, write a function that returns a list of the strings
 //at even indices i.e index 2,4,6 etc
-fun names(names:List<String>):List<String{
-    names.forEachIndexed{ index, y ->
-        if (index % 2==0)
+fun namesList(names:List<String>):List<String> {
+    names.forEachIndexed { index, y ->
+        if (index % 2 == 0) {
             println(y)
+        }
     }
+    return names
+}
+
 //Given a list of people’s heights in metres. Write a function that returns
 //the average height and the total height
 
@@ -30,9 +36,31 @@ fun heights(){
      }
 //Given a list of Person objects, each with the attributes, name, age,
 //height and weight. Sort the list in order of descending age
+class Person(var Name: String,var age: Int,var height: Double,var weight: Int)
+//Given a list similar to the one above, write a function in which you will
+//create 2 more people objects and add them to the list at one go.
 
- data class Person(var Name:String,var age:Int,var height:Double,var weight:Double) {
-}
+//Write a function that takes in a list of Car objects each with a
+//registration and mileage attribute and returns the average mileage of
+//all the vehicles in the list.
+data class Car(var registration:String,var mileage:Int)
+fun namesOfCars(motors:List<Car>):Int {
+    var average = 0
+    motors.forEach { x ->
+        x.mileage
+        average += x.mileage}
+        var averageTotal = motors.count()
+        return averageTotal
+
+    }
+
+
+
+
+
+
+
+
 
 
 
